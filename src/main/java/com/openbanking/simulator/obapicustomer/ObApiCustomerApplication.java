266f1e7bf69179a7e5dcf3d99c5976dc72229c20
Madan Narra <narra.madan@outlook.com>
2020-09-22 07:39:14 +1000

    Initial Commit

---
+++ src/main/java/com/openbanking/simulator/obapicustomer/ObApiCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapicustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObApiCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObApiCustomerApplication.class, args);
+	}
+
+}
